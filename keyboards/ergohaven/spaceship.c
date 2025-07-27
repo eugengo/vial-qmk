0b8ec99954dd45b162ce03e2cb22c179d1ab2cc2
Eugen Trippel <1540990+eugengo@users.noreply.github.com>
2025-07-27 19:56:08 +0200

    Add spaceship splash screen image

--- keyboards/ergohaven/spaceship.c
+++ keyboards/ergohaven/spaceship.c
@@ -201,6 +201,7 @@ static const char PROGMEM mask_row_4[] = {
 };
 
 static void render_spaceship(void) {
+    oled_set_rotation(OLED_ROTATION_180);
     char wpm = get_current_wpm();
     char render_row[128];
     int i;
