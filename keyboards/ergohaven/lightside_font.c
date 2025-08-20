9b3578d9d4e2d5a87d2bf5f641933075202f5d3d
Eugen Trippel <1540990+eugengo@users.noreply.github.com>
2025-08-20 11:26:52 +0200

    Added new font

--- keyboards/ergohaven/lightside_font.c
+++ keyboards/ergohaven/lightside_font.c
@@ -4,6 +4,7 @@
 // Online editor: https://helixfonteditor.netlify.com/
 // See also: https://github.com/soundmonster/glcdfont_converter
 
+
 const unsigned char font[] PROGMEM = {
     0xC0, 0xE3, 0xF6, 0xF6, 0xEC, 0xB2,
     0x30, 0xE0, 0xC0, 0x00, 0x00, 0x00,
