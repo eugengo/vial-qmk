c1f56d532dd472c7a673c883795891cad50b686f
Eugen Trippel <1540990+eugengo@users.noreply.github.com>
2025-08-25 14:12:31 +0200

    new pics

--- keyboards/ergohaven/ergohaven_oled.c
+++ keyboards/ergohaven/ergohaven_oled.c
@@ -193,9 +193,9 @@ void render_status_darkside(bool is_master) {
     render_mod_pair(mods, MOD_MASK_CTRL, MOD_MASK_SHIFT, 6, 4);
 
     oled_set_cursor(0, 11);
-    oled_write_P(PSTR("\x06\x07\x08\x09\x0a"), false);
-    oled_write_P(PSTR("\x0b\x0c\x0d\x0e\x0f"), false);
-    oled_write_P(PSTR("\x20\x20\x20\x20\x10"), false);
+    oled_write_P(PSTR("\x06\x07\x08\x0a"), false);
+    oled_write_P(PSTR("\x0b\x0c\x0d\x0f"), false);
+    oled_write_P(PSTR("\x20\x20\x20\x10"), false);
 
     oled_set_cursor(0, 14);
     static uint32_t start_time = 0;
